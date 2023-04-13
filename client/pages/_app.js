351fbda861942cbe86a90351672af4780480174f
dudefromhell <armine.s888@gmail.com>
2023-04-13 17:35:44 +0330

    init

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Chat app</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="chat app developed by Armin Esmaeili Malek"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
